4f27a4974e1395e28e650db51937ad6fc58c9f92
qihua169 <qihua169@126.com>
2026-02-06 09:43:22 +0800

    zy

---
+++ js/量子资源[资].js
@@ -0,0 +1,22 @@
+/*
+@header({
+  searchable: 2,
+  filterable: 0,
+  quickSearch: 1,
+  title: '量子资源[资]',
+  '类型': '影视',
+  lang: 'ds'
+})
+*/
+
+// https://cj.lziapi.com/api.php/provide/vod/?ac=list
+
+var rule = {
+    模板: '采集1',
+    title: '量子资源[资]',
+    host: 'https://cj.lziapi.com',
+    // homeTid: '13',
+    homeTid: '',
+    cate_exclude: '电影片|连续剧|综艺片|动漫片|电影解说|体育|演员|新闻资讯|伦理片|海外动漫|港台动漫|欧美动漫|日韩动漫|欧美综艺|日韩综艺|港台综艺|泰国剧|海外剧|日本剧|台湾剧|欧美剧|韩国剧',
+    parse_url: '',
+}
---
+++ js/非凡资源[资].js
@@ -0,0 +1,11 @@
+
+var rule = {
+    模板: '采集1',
+    title: '非凡资源[资]',
+    host: 'http://cj.ffzyapi.com',
+    // homeTid: '13',
+    homeTid: '',
+    cate_exclude: '电影片|连续剧|综艺片|动漫片|电影解说|体育|演员|新闻资讯|伦理片|海外动漫|港台动漫|欧美动漫|日韩动漫|欧美综艺|日韩综艺|港台综艺|泰国剧|海外剧|日本剧|台湾剧|欧美剧|韩国剧',
+    // parse_url: 'json:http://jx.84jia.com/m3u8ts.php?url=',
+    parse_url: '',
+}
